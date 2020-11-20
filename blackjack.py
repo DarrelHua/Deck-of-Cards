2922f154a2a0303ebcc5b77fe8948fc744e6de0c
DarrelHua <74470773+DarrelHua@users.noreply.github.com>
2020-11-19 16:20:21 -0800

    made a pleyer,deck and dealer

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,20 @@
+from Deck_of_cards import Deck
+from Deck_of_cards import Card
+
+
+#First I need to create a deck and shuffle the cards
+
+deck = Deck()
+deck.shuffle()
+
+#make the dealer and player
+player = []
+dealer = []
+
+#Create the game of blackjack 
+while len(player) < 2:
+    player.append(deck.drawCard)
+
+print(player)
+
 
